ab1de04799dd51bb770b54e2110f2c103594ce17
luca-heitmann <luca.marco.heitmann@gmail.com>
2023-08-30 10:23:02 +0200

    workaround for arm build

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-docker_repo="lucaheitmann/foodpollbot:1.0.0"
+docker_repo="ghcr.io/luca-heitmann/foodpollbot:1.0.0" # FIXME version needs to be changed in Dockerfile.arm64 too, because gradle gets stuck when building with arm image directly
 
-docker build --platform linux/arm64 -t "$docker_repo" .
-docker push "$docker_repo"
+docker build -f Dockerfile.amd64 -t "${docker_repo}-amd64" .
+docker build -f Dockerfile.arm64 --platform linux/arm64 -t "${docker_repo}-arm64" .
+docker push "${docker_repo}-amd64"
+docker push "${docker_repo}-arm64"
